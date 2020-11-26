540e17582ef7f10276bfdf45784a7bdbc8482a93
Dawil69hehe <dawidwilemski@gmail.com>
2020-11-26 17:18:57 +0100

    Dodanie funkcjonalności obliczenia sumy itd itd

--- src/test.java
+++ src/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("yyyeee");
+        int a = 5;
+        int b = 3;
+        System.out.println(a+b);
     }
 }
